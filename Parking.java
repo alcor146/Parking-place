0b16f09f91b1447887a6a8073b09d218714d961e
alcor146 <74672415+alcor146@users.noreply.github.com>
2021-02-25 16:34:31 +0200

    Update Parking.java

--- Parking.java
+++ Parking.java
@@ -5,6 +5,7 @@ public class Parking extends Parking_spot {
     private int capacitate = 10;
 
     public Parking(){
+
         this.spots = new ArrayList<Parking_spot>();
         for(int i = 0; i < this.capacitate; i++)
             this.spots.add(new Parking_spot());
@@ -29,11 +30,13 @@ public class Parking extends Parking_spot {
     }
 
     public void freeSpot(String nrInmatriculare){
+
         for(int i=0; i<this.capacitate; i++){
-            if(this.spots.get(i).getNrInmatriculare().equals(nrInmatriculare) && this.spots.get(i).getStatus() == true ){
-                System.out.println("1 car left the parking spot");
-                this.spots.get(i).exitSpot();
-                
+            if(this.spots.get(i).getStatus() == true ){
+                if(this.spots.get(i).getNrInmatriculare().equals(nrInmatriculare)){
+                    System.out.println("1 car left the parking spot");
+                    this.spots.get(i).exitSpot();
+                }
             }
         }
         System.out.println("This specific car was not found");
@@ -42,6 +45,7 @@ public class Parking extends Parking_spot {
 
 
     public Parking_spot findSpotAvailable(){
+
         for(int i=0; i<this.spots.size(); i++)
             if(this.spots.get(i).getStatus() == false)
                 return this.spots.get(i);
@@ -65,7 +69,5 @@ public class Parking extends Parking_spot {
     public void setSpots(ArrayList<Parking_spot> spots) {
         this.spots = spots;
     }
-
-
 
 }
